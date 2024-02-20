bd19ebff9457311d59e478fca1cad885325e0a2b
viridian <wi214905@ucf.edu>
2024-02-20 09:12:25 -0500

    note taking today

--- function_practice/function_practice.js
+++ function_practice/function_practice.js
@@ -34,10 +34,10 @@ function myFunc(theObject) {
   myFunc(mycar);
   console.log(mycar.make); // "Toyota"
 
-  When you pass an array as a parameter, if the function changes any of the array's values, that change is visible outside the function, as shown in the following example:
+//   When you pass an array as a parameter, if the function changes any of the array's values, that change is visible outside the function, as shown in the following example:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function myFunc(theArr) {
   theArr[0] = 30;
 }
@@ -54,11 +54,11 @@ console.log(arr[0]); // 30
 
 // Such a function can be anonymous; it does not have to have a name. For example, the function square could have been defined as:
 
-const square = function (number) {
+const square_1 = function (number) {
   return number * number;
 };
 
-console.log(square(4)); // 16
+console.log(square_1(4)); // 16
 
 // However, a name can be provided with a function expression. Providing a name allows the function to refer to itself, and also makes it easier to identify the function in a debugger's stack traces:
 
@@ -90,9 +90,10 @@ console.log(map(cube, numbers)); // [0, 1, 8, 125, 1000]
 // In JavaScript, a function can be defined based on a condition. For example, the following function definition defines myFunc only if num equals 0:
 
 
-let myFunc;
+let myFunc_1;
+let num = 0
 if (num === 0) {
-  myFunc = function (theObject) {
+    myFunc_1 = function (theObject) {
     theObject.make = "Toyota";
   };
 }
@@ -120,59 +121,59 @@ square(5);
 // A function can call itself. For example, here is a function that computes factorials recursively:
 
 
-function factorial(n) {
+function factorial_1(n) {
   if (n === 0 || n === 1) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return n * factorial_1(n - 1);
   }
 }
 
 // You could then compute the factorials of 1 through 5 as follows:
 
 
-console.log(factorial(1)); // 1
-console.log(factorial(2)); // 2
-console.log(factorial(3)); // 6
-console.log(factorial(4)); // 24
-console.log(factorial(5)); // 120
+console.log(factorial_1(1)); // 1
+console.log(factorial_1(2)); // 2
+console.log(factorial_1(3)); // 6
+console.log(factorial_1(4)); // 24
+console.log(factorial_1(5)); // 120
 
-Function hoisting
-Consider the example below:
+// Function hoisting
+// Consider the example below:
 
-JS
-Copy to Clipboard
-console.log(square(5)); // 25
+console.log("\n\n");
+
+console.log(square_1(5)); // 25
 
 function square(n) {
   return n * n;
 }
-This code runs without any error, despite the square() function being called before it's declared. This is because the JavaScript interpreter hoists the entire function declaration to the top of the current scope, so the code above is equivalent to:
+// This code runs without any error, despite the square() function being called before it's declared. This is because the JavaScript interpreter hoists the entire function declaration to the top of the current scope, so the code above is equivalent to:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 // All function declarations are effectively at the top of the scope
 function square(n) {
   return n * n;
 }
 
 console.log(square(5)); // 25
-Function hoisting only works with function declarations — not with function expressions. The following code will not work:
+// Function hoisting only works with function declarations — not with function expressions. The following code will not work:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 console.log(square(5)); // ReferenceError: Cannot access 'square' before initialization
-const square = function (n) {
+ square = function (n) {
   return n * n;
 };
 
-Function scope
-Variables defined inside a function cannot be accessed from anywhere outside the function, because the variable is defined only in the scope of the function. However, a function can access all variables and functions defined inside the scope in which it is defined.
+// Function scope
+// Variables defined inside a function cannot be accessed from anywhere outside the function, because the variable is defined only in the scope of the function. However, a function can access all variables and functions defined inside the scope in which it is defined.
 
-In other words, a function defined in the global scope can access all variables defined in the global scope. A function defined inside another function can also access all variables defined in its parent function, and any other variables to which the parent function has access.
+// In other words, a function defined in the global scope can access all variables defined in the global scope. A function defined inside another function can also access all variables defined in its parent function, and any other variables to which the parent function has access.
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 // The following variables are defined in the global scope
 const num1 = 20;
 const num2 = 3;
@@ -199,31 +200,31 @@ function getScore() {
 
 console.log(getScore()); // "Chamakh scored 5"
 
-Recursion
-A function can refer to and call itself. There are three ways for a function to refer to itself:
+// Recursion
+// A function can refer to and call itself. There are three ways for a function to refer to itself:
 
-The function's name
-arguments.callee
-An in-scope variable that refers to the function
-For example, consider the following function definition:
+// The function's name
+// arguments.callee
+// An in-scope variable that refers to the function
+// For example, consider the following function definition:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 const foo = function bar() {
   // statements go here
 };
-Within the function body, the following are all equivalent:
+// Within the function body, the following are all equivalent:
 
 bar()
 arguments.callee()
 foo()
 
-A function that calls itself is called a recursive function. In some ways, recursion is analogous to a loop. Both execute the same code multiple times, and both require a condition (to avoid an infinite loop, or rather, infinite recursion in this case).
+// A function that calls itself is called a recursive function. In some ways, recursion is analogous to a loop. Both execute the same code multiple times, and both require a condition (to avoid an infinite loop, or rather, infinite recursion in this case).
 
-However, some algorithms cannot be simple iterative loops. For example, getting all the nodes of a tree structure (such as the DOM) is easier via recursion:
+// However, some algorithms cannot be simple iterative loops. For example, getting all the nodes of a tree structure (such as the DOM) is easier via recursion:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function walkTree(node) {
   if (node === null) {
     return;
@@ -238,21 +239,21 @@ function walkTree(node) {
 
 // Since a nested function is a closure, this means that a nested function can "inherit" the arguments and variables of its containing function. In other words, the inner function contains the scope of the outer function.
 
-The inner function can be accessed only from statements in the outer function.
-The inner function forms a closure: the inner function can use the arguments and variables of the outer function, while the outer function cannot use the arguments and variables of the inner function.
+// The inner function can be accessed only from statements in the outer function.
+// The inner function forms a closure: the inner function can use the arguments and variables of the outer function, while the outer function cannot use the arguments and variables of the inner function.
 
-Multiply-nested functions
-Functions can be multiply-nested. For example:
+// Multiply-nested functions
+// Functions can be multiply-nested. For example:
 
-A function (A) contains a function (B), which itself contains a function (C).
-Both functions B and C form closures here. So, B can access A, and C can access B.
-In addition, since C can access B which can access A, C can also access A.
-Thus, the closures can contain multiple scopes; they recursively contain the scope of the functions containing it. This is called scope chaining. (The reason it is called "chaining" is explained later.)
+// A function (A) contains a function (B), which itself contains a function (C).
+// Both functions B and C form closures here. So, B can access A, and C can access B.
+// In addition, since C can access B which can access A, C can also access A.
+// Thus, the closures can contain multiple scopes; they recursively contain the scope of the functions containing it. This is called scope chaining. (The reason it is called "chaining" is explained later.)
 
-Consider the following example:
+// Consider the following example:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function A(x) {
   function B(y) {
     function C(z) {
@@ -265,15 +266,15 @@ function A(x) {
 A(1); // Logs 6 (which is 1 + 2 + 3)
 
 
-Default parameters
-In JavaScript, parameters of functions default to undefined. However, in some situations it might be useful to set a different default value. This is exactly what default parameters do.
+// Default parameters
+// In JavaScript, parameters of functions default to undefined. However, in some situations it might be useful to set a different default value. This is exactly what default parameters do.
 
-In the past, the general strategy for setting defaults was to test parameter values in the body of the function and assign a value if they are undefined.
+// In the past, the general strategy for setting defaults was to test parameter values in the body of the function and assign a value if they are undefined.
 
-In the following example, if no value is provided for b, its value would be undefined when evaluating a*b, and a call to multiply would normally have returned NaN. However, this is prevented by the second line in this example:
+// In the following example, if no value is provided for b, its value would be undefined when evaluating a*b, and a call to multiply would normally have returned NaN. However, this is prevented by the second line in this example:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function multiply(a, b) {
   b = typeof b !== "undefined" ? b : 1;
   return a * b;
@@ -281,16 +282,16 @@ function multiply(a, b) {
 
 console.log(multiply(5)); // 5
 
-Arrow functions
-An arrow function expression (also called a fat arrow to distinguish from a hypothetical -> syntax in future JavaScript) has a shorter syntax compared to function expressions and does not have its own this, arguments, super, or new.target. Arrow functions are always anonymous.
+// Arrow functions
+// An arrow function expression (also called a fat arrow to distinguish from a hypothetical -> syntax in future JavaScript) has a shorter syntax compared to function expressions and does not have its own this, arguments, super, or new.target. Arrow functions are always anonymous.
 
-Two factors influenced the introduction of arrow functions: shorter functions and non-binding of this.
+// Two factors influenced the introduction of arrow functions: shorter functions and non-binding of this.
 
-Shorter functions
-In some functional patterns, shorter functions are welcome. Compare:
+// Shorter functions
+// In some functional patterns, shorter functions are welcome. Compare:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 const a = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
 
 const a2 = a.map(function (s) {
@@ -302,11 +303,11 @@ console.log(a2); // [8, 6, 7, 9]
 const a3 = a.map((s) => s.length);
 
 console.log(a3); // [8, 6, 7, 9]
-No separate this
-Until arrow functions, every new function defined its own this value (a new object in the case of a constructor, undefined in strict mode function calls, the base object if the function is called as an "object method", etc.). This proved to be less than ideal with an object-oriented style of programming.
+// No separate this
+// Until arrow functions, every new function defined its own this value (a new object in the case of a constructor, undefined in strict mode function calls, the base object if the function is called as an "object method", etc.). This proved to be less than ideal with an object-oriented style of programming.
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function Person() {
   // The Person() constructor defines `this` as itself.
   this.age = 0;
@@ -320,10 +321,10 @@ function Person() {
 }
 
 const p = new Person();
-In ECMAScript 3/5, this issue was fixed by assigning the value in this to a variable that could be closed over.
+// In ECMAScript 3/5, this issue was fixed by assigning the value in this to a variable that could be closed over.
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function Person() {
   // Some choose `that` instead of `self`.
   // Choose one and be consistent.
@@ -336,12 +337,12 @@ function Person() {
     self.age++;
   }, 1000);
 }
-Alternatively, a bound function could be created so that the proper this value would be passed to the growUp() function.
+// Alternatively, a bound function could be created so that the proper this value would be passed to the growUp() function.
 
-An arrow function does not have its own this; the this value of the enclosing execution context is used. Thus, in the following code, the this within the function that is passed to setInterval has the same value as this in the enclosing function:
+// An arrow function does not have its own this; the this value of the enclosing execution context is used. Thus, in the following code, the this within the function that is passed to setInterval has the same value as this in the enclosing function:
 
-JS
-Copy to Clipboard
+console.log("\n\n");
+
 function Person() {
   this.age = 0;
 
@@ -350,4 +351,4 @@ function Person() {
   }, 1000);
 }
 
-const p = new Person();
+p = new Person();
